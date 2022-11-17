f46065680eb975b739acb81d61f53728cdc0d25d
David Francisco <dfrancisco2677@gmail.com>
2022-11-16 20:37:18 -0800

    Logo now fades in.

--- pages/Components/Banner.jsx
+++ pages/Components/Banner.jsx
@@ -4,7 +4,7 @@ import styles from '../../styles/Banner.module.css';
 export default function Banner() {
     return (
         <div className={styles.banner}>
-            <Image className='banner-image' src='/../public/assets/banner-logo-png.png' alt='banner' width={500} height={200} />
+            <Image className={styles.Image} src='/../public/assets/banner-logo-png.png' alt='banner' width={500} height={200} />
         </div>
     );
     }
