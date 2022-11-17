2b36cfa221c37d47e13cc7c975d7ed6d0d94ec84
David Francisco <dfrancisco2677@gmail.com>
2022-11-16 20:21:32 -0800

    Added proper module-css to Banner component.

--- pages/Components/Banner.jsx
+++ pages/Components/Banner.jsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
+import styles from '../../styles/Banner.module.css';
 
 export default function Banner() {
     return (
-        <div className="banner">
+        <div className={styles.banner}>
             <Image className='banner-image' src='/../public/assets/banner-logo-png.png' alt='banner' width={500} height={200} />
         </div>
     );
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import '../styles/Banner.module.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
