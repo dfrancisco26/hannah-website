88afd05a146d73ecfa4c53ee4b270ade9022cd14
David  Francisco <davidfrancisco@Davids-MacBook-Air.local>
2022-11-16 19:35:35 -0800

    initial commit

---
+++ pages/Components/Banner.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Banner() {
+    return (
+        <div className="banner">
+        <p>THIS IS WHERE THE BANNER SHOULD BE</p>
+        </div>
+    );
+    }
---
+++ pages/Components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <div className="footer">
+        <p>THIS IS WHERE THE FOOTER SHOULD BE</p>
+        </div>
+    );
+}
+    
